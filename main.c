501685ef7b442c81d6aecca4670b8925e5dbc510
dressel <dressel@stanford.edu>
2015-02-10 11:01:09 -0800

    Allow user to specify degree this occurs at via command line

--- main.c
+++ main.c
@@ -14,6 +14,14 @@
 /* Main function */
 int main(int argc, char *argv[])
 {
+	/* Specify the degree */
+	int degree = 0;
+	if (argc == 2)
+	{
+		//printf("arg is: %s\n", argv[1]);
+		degree = atoi(argv[1]);
+	}
+
 	/* Start the connection to the WiFly */
 	/* If the connection does not exist, quit */
 	int fd = startconnection();
@@ -28,7 +36,7 @@ int main(int argc, char *argv[])
 
 	/* Open a file to write values to */
 	/* Appending values */
-	FILE *f = fopen("wiflytest.txt", "a");
+	FILE *f = fopen("wiflytest.csv", "a");
 	if (f == NULL)
 	{
 		printf("Error opening file\n");
@@ -36,10 +44,11 @@ int main(int argc, char *argv[])
 	}
 
 	/* Scan values to this file */
+	/* Add degree at which you measure first */
+	fprintf(f, "%i,", degree);
 	scanrssi_f(fd, "JAMMER01", f, 10);
 
 	/* Be sure to close the output file and connection */
 	fclose(f);
 	close(fd);
 }
-
