957815f5976a7c5d2259eb490167db9f80c51f56
dressel <dressel@stanford.edu>
2015-02-10 10:36:06 -0800

    forgot an endline comma

--- serialwifly.h
+++ serialwifly.h
@@ -7,7 +7,7 @@ int commandmode(int);
 
 /* Scan the channels for a specific SSID */
 int scanrssi(int, char *);
-int scanrssi_f(int fd, char *ssid, FILE *f, int numtimes)
+int scanrssi_f(int fd, char *ssid, FILE *f, int numtimes);
 
 /* Parses output from scan  */
 int getrssi(char *, char *);
