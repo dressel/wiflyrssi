75f39a486fbfe5c9e530018a3a1547dcac204b03
Louis Dressel <dressel@stanford.edu>
2015-02-14 16:12:31 -0800

    Updates to how connections are made

--- serialwifly.c
+++ serialwifly.c
@@ -4,21 +4,34 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-
 #include <limits.h>
 
+// TODO: Investigate this parameter... what exactly does it do?
 #define WD 1000
 
 
+/**
+ * Start serial connection over one some port
+ *
+ * *port should be of the format "/dev/ttyUSB0"
+ */
+int start_connection(char *port)
+{
+	int fd = open(port, O_RDWR | O_NOCTTY | O_NDELAY);
+	return fd;
+}
+
 
 /**
- * Start the connection to /dev/ttyUSB0
- * TODO: Allow user to specify. Maybe it isn't /dev/ttyUSB0
+ * Quickly connect to one of the USB ports.
  */
-int startconnection()
+int connect0()
 {
-	int fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
-	return fd;
+	return start_connection("/dev/ttyUSB0");
+}
+int connect1()
+{
+	return start_connection("/dev/ttyUSB1");
 }
 
 
@@ -31,18 +44,17 @@ int scanrssi(int fd, char *ssid)
 	write(fd, "scan 10\r", 8);
 	usleep(1000000);
 	read(fd, buf, sizeof(buf));
-	//printf("buf is:\n%s\n", buf);
 
 	return getrssi(buf, ssid);
 }
+
 
 /**
  * Scans numtimes times and prints rssi values to a line in a file
  */
 int scanrssi_f(int fd, char *ssid, FILE *f, int numtimes)
 {
-	int rssi_value;
-	int i;
+	int rssi_value, i;
 	for (i = 1; i <= (numtimes-1); i++)
 	{
 		rssi_value = scanrssi(fd, ssid);
@@ -51,15 +63,41 @@ int scanrssi_f(int fd, char *ssid, FILE *f, int numtimes)
 	}
 
 	/* Don't include comma for last one */
-	/* TODO: I end with a newline. Should I? */
 	rssi_value = scanrssi(fd, ssid);
 	fprintf(f, "%i\n", rssi_value);
 	printf("rssi_value = %i\n", rssi_value);
 }
 
+
+/**
+ * Scans numtimes times and prints rssi values to a line in a file
+ */
+int scanrssi_2(int fd1, int fd2, char *ssid, FILE *f1, FILE *f2, int numtimes)
+{
+	int rssi_1, rssi_2, i;
+	for (i = 1; i <= (numtimes-1); i++)
+	{
+		rssi_1 = scanrssi(fd1, ssid);
+		rssi_2 = scanrssi(fd2, ssid);
+		fprintf(f1, "%i,", rssi_1);
+		fprintf(f2, "%i,", rssi_2);
+		printf("Wifly 1 RSSI = %i\n", rssi_1);
+		printf("Wifly 2 RSSI = %i\n", rssi_2);
+	}
+
+	/* Don't include comma for last one */
+	rssi_1 = scanrssi(fd1, ssid);
+	rssi_2 = scanrssi(fd2, ssid);
+	fprintf(f1, "%i\n", rssi_1);
+	fprintf(f2, "%i\n", rssi_2);
+	printf("Wifly 1 RSSI = %i\n", rssi_1);
+	printf("Wifly 2 RSSI = %i\n", rssi_2);
+}
+
+
 /**
  * This puts you in command mode
- * Must wait 250 
+ * Must wait 250 ms before
  */
 int commandmode(int fd)
 {
@@ -70,9 +108,9 @@ int commandmode(int fd)
 	write(fd, "exit\r", 5);
 	usleep(WD);
 
-	// Give it the turn on command
-	/* must wait 250 ms before and after giving $$$ command */
-	/* to be safe, i wate 300 ms */
+	/* Give it the turn on command */
+	/* Must wait 250 ms before and after giving $$$ command */
+	/* To be safe, I wait 300 ms */
 	write(fd, "$$$", 3);
 	usleep(300000);
 }
@@ -82,7 +120,6 @@ int commandmode(int fd)
  * Searches for SSID provided.
  * Returns the RSSI value of that SSID, as an int.
  */
-// TODO: Should this be static?
 int getrssi(char *buf, char *ssid)
 {
 	char *delim = "\n";
